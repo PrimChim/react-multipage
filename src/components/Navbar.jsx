f8fd91a5bbb5f815e5f42b5cb5f34384a206bb3e
PrimChim <gpritam922@gmail.com>
2024-12-07 17:19:09 +0545

    updated: blogger website clone made

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,30 @@
 import { Link } from 'react-router-dom';
+import bloggerLogo from '../assets/images/blogger.webp';
 
 export default function Navbar() {
     return (
         <nav>
-            <Link to="/">Root</Link>
-            <Link to="/new">New</Link>
-            <Link to="/blogs">Blogs</Link>
+            <Link
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    fontSize: "1.3rem",
+                }}
+                to="/"
+            >
+                <img
+                    src={bloggerLogo}
+                    alt="Blogger logo"
+                    style={{ height: "35px", marginRight: "10px" }}
+                />
+                Blogger
+            </Link>
+            <div>
+                {/* <Link to="/new">New</Link>
+                <Link to="/blogs">Blogs</Link> */}
+                <Link to="">SIGN IN</Link>
+            </div>
         </nav>
     );
 }
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,7 +1,23 @@
+import '../assets/css/Root.css'
+
 export default function Root(){
     return(
         <>
-            <h1>This is root page</h1>
+        <div className="main">
+            <h1>Publish your passions, your way</h1>
+            <p>Create a unique and beautiful blog easily.</p>
+            <button style={{
+                textTransform: "uppercase",
+                padding: ".875rem 1.25rem",
+                border: "none",
+                backgroundColor: "#ff9224",
+                color: "rgb(255, 255, 255)",
+                borderRadius: "4px",
+                fontWeight: '700',
+                height: '50px',
+                fontSize: '.8rem',
+            }}>Create Your Blog</button>
+        </div>
         </>
     )
 }
